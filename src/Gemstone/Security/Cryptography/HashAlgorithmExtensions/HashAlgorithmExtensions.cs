f52a0987daaec0131b768106e6ed13e7ba745874
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2020-01-03 17:11:48 -0500

    Added HashAlgorithmExtensions\

---
+++ src/Gemstone/Security/Cryptography/HashAlgorithmExtensions/HashAlgorithmExtensions.cs
@@ -0,0 +1,44 @@
+﻿//******************************************************************************************************
+//  HashAlgorithmExtensions.cs - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/03/2020 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Gemstone.Security.Cryptography.HashAlgorithmExtensions
+{
+    /// <summary>
+    /// Defines extension functions related to cryptographic <see cref="HashAlgorithm"/> objects.
+    /// </summary>
+    public static class HashAlgorithmExtensions
+    {
+        /// <summary>
+        /// Gets the Base64 encoded hash of the provided string <paramref name="value"/>.
+        /// </summary>
+        /// <param name="algorithm"><see cref="SymmetricAlgorithm"/> to use for encryption.</param>
+        /// <param name="value">String value to hash.</param>
+        /// <returns>Base64 encoded hash of provided string <paramref name="value"/>.</returns>
+        public static string GetStringHash(HashAlgorithm algorithm, string value) => 
+            value == null ? null : Convert.ToBase64String(algorithm.ComputeHash(Encoding.UTF8.GetBytes(value)));
+    }
+}
