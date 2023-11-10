7d0e904fb2d3b85582717ceb3b4c82aa40c40709
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2023-11-10 11:15:55 -0600

    Added Gemstone.POSIX

---
+++ src/Gemstone.POSIX/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Define absolute path for script log
+log=${PWD}/Gemstone.POSIX-build-log.txt
+
+# destDir=..
+
+if [ "$(id -u)" != "0" ]; then
+    echo "ERROR: Operation must execute as root." 1>&2
+    exit 1
+fi
+
+echo
+echo "Make sure gcc compiler and pam libraries are installed, e.g.:"
+echo "    > sudo apt install build-essential"
+echo "    > sudo apt install libpam0g-dev"
+echo
+read -p "Are you ready to continue (y/N)? " -r -n 1
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+# Create log file
+echo "Gemstone.POSIX Build Log" > ${log}
+date >> ${log}
+
+if [ ${OSTYPE} != ${OSTYPE#darwin} ]; then
+    echo "Mac operating system detected..." >> ${log}
+    isMac=1
+else
+    echo "Linux operating system assumed..." >> ${log}
+    isMac=0
+fi
+
+# cd Gemstone.POSIX/ >> ${log}
+
+# Build Gemstone POSIX library
+if [ $isMac -ne 0 ]; then
+    sudo gcc -m32 -c -Wall -Werror -fpic Gemstone.POSIX.c >> ${log}
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to compile Gemstone POSIX library"
+        exit 1
+    fi
+
+    sudo gcc -m32 -dynamiclib -undefined suppress -flat_namespace Gemstone.POSIX.o -o Gemstone.POSIX.so -lpam >> ${log}
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to build Gemstone POSIX dynamic library"
+        exit 1
+    fi
+else
+    gcc -c -Wall -Werror -fpic Gemstone.POSIX.c >> ${log}
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to compile Gemstone POSIX library"
+        exit 1
+    fi
+
+    gcc -shared -o Gemstone.POSIX.so Gemstone.POSIX.o -lpam -lpam_misc -lcrypt >> ${log}
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to build Gemstone POSIX shared library"
+        exit 1
+    fi
+fi
+
+# cp -v Gemstone.POSIX.so ${destDir} >> ${log}
