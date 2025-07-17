889f66b7fa5d56e4e5af6ccf78fdc435e9ac76c2
prestoncraw <prestoncrawford530@gmail.com>
2025-07-17 11:35:49 -0400

    added JArrayExtensions and MergeByKey method

---
+++ src/Gemstone/JArrayExtensions/JArrayExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Gemstone.JArrayExtensions
+{
+    /// <summary>
+    /// Defines extension methods for JArray.
+    /// </summary>
+    public static class JArrayExtensions
+    {
+        /// <summary>
+        /// Rebuilds original array so that items from originalArray are replaced by any same‑key items in the override array
+        /// </summary>
+        public static JArray MergeByKey(this JArray originalArray, JArray overrideArray, string uniqueKeyField)
+        {
+            // 1) Build lookup from original
+            Dictionary<string, JObject> lookup = originalArray
+                .OfType<JObject>()
+                .Where(o => o[uniqueKeyField] is not null)
+                .ToDictionary(o => o[uniqueKeyField]!.ToString()!, o => o);
+
+            // 2) Override
+            foreach (JObject item in overrideArray.OfType<JObject>())
+            {
+                string? key = item[uniqueKeyField]?.ToString();
+                if (!string.IsNullOrEmpty(key) && lookup.ContainsKey(key))
+                    lookup[key] = item;
+            }
+
+            return new JArray(lookup.Values);
+        }
+
+    }
+}
